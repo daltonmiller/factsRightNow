530f3acc003adb532b1a169dc59803ea9020702e
Dalton Miller <daltonmiller1145@Daltons-MacBook-Pro.local>
2020-10-26 18:04:36 -0600

    heroku

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -47,7 +47,7 @@ const Questions = () => {
     }, [formState])
 
     useEffect((item) => {
-    axios.get(`http://localhost:5800/questions`)
+    axios.get(`https://facts-right-now.herokuapp.com/questions`)
     .then(res => {
         // console.log('request', res)
         setQuestions(res.data)
