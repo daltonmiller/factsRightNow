0dbbc54b9d2d695e8407ad0d288851e9fe229f68
Dalton Miller <daltonmiller1145@Daltons-MacBook-Pro.local>
2020-10-26 18:33:10 -0600

    touched up

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Questions from './components/Questions'
 function App() {
   return (
     <div className="App">
+      <script data-ad-client="ca-pub-4124318818367381" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <Questions />
     </div>
   );
